321cc1073b85297b2244f8c85666fc3357367261
Glacier <1556802883@qq.com>
2024-11-07 09:42:00 +0800

    模型

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+"""
+    1.我们的模型需要继承自models.Model
+    2.系统自动添加主键--id
+    3.字段
+        字段名=model.类型（选项）
+        字段名其实就是数据表的属性
+        字段名不要使用python，mysql关键字
+"""
 # Create your models here.
+class BookInfo(models.Model):
+    # id
+    name = models.CharField(max_length=10)
+
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=10)
+    gender = models.BooleanField()
+    # 外键约束：人类属于哪本书
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
