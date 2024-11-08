f6609080b3e0d617c270d3d8a42d40d7ef3472a0
Glacier <1556802883@qq.com>
2024-11-08 10:21:35 +0800

    模板的基本使用

--- bookmanager/book/urls.py
+++ bookmanager/book/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from book.views import index
 # 固定写法urlpatterns = []
 urlpatterns = [
-    path('index', index),
+    path('index/', index),
 ]
--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -12,4 +12,14 @@ from django.http import HttpResponse
 
 def index(request):
 
-    return HttpResponse('ok')
+    # 1.return HttpResponse('ok')
+    # 2.return render(request, 'book/index.html')
+    # render 渲染模版
+    # render参数:request 请求, template_name 模版名字, context=None 将视图中的数据传给html(模板)
+    # 3.模拟数据查询
+    # context=  1)将视图中的数据传给html(模板)
+    # 2) html(模板)采用{{ 变量 }} 形式来展示数据
+    context = {
+        'name': '马上双十一，点击有惊喜'
+    }
+    return render(request, 'book/index.html',context=context)
