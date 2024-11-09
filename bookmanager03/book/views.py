6002bc5abd8f38db99ddf2607b35eb75f7906c9c
Glacier <1556802883@qq.com>
2024-11-09 10:25:28 +0800

    发送post请求

--- bookmanager03/book/views.py
+++ bookmanager03/book/views.py
@@ -11,4 +11,17 @@ def create_book(request):
         readcount=10
     )
     return HttpResponse('create')
-    
+
+def shop(request,city_id,shop_id):
+    # print(city_id,shop_id)
+    query_params = request.GET.get('order')
+    print(query_params)
+    # http://localhost:8000/10010/10011/?order=readcount
+    # ?后面是get请求方式
+    # 打印结果：readcount
+    return HttpResponse('齐哥的小饭店')
+
+def register(request):
+    data=request.POST
+    print(data)
+    return HttpResponse('ok')
