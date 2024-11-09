40aea406193401002a93eec9405b98e8a1f9cb56
Glacier <1556802883@qq.com>
2024-11-09 10:52:01 +0800

    json数据的请求和发送

--- bookmanager03/book/urls.py
+++ bookmanager03/book/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from book.views import create_book,shop,register
+from book.views import create_book,shop,register,json
 
 urlpatterns = [
     path('create/', create_book),
     path('<city_id>/<shop_id>/', shop),
     path('register/',register),
+    path('json/',json),
 ]
--- bookmanager03/book/views.py
+++ bookmanager03/book/views.py
@@ -25,3 +25,23 @@ def register(request):
     data=request.POST
     print(data)
     return HttpResponse('ok')
+
+def json(request):
+    # json数据不能通过request.POST获取
+    body=request.body
+    print(body)
+    # b'{\r\n    "name":"itcast",\r\n    "age":10\r\n\r\n}'
+    body_str=body.decode()
+    print(body_str)
+    # {
+        # "name":"itcast",
+        # "age":10
+    # }
+    # print(type(body_str))
+    """
+    json形式的字符串可以转换成Python的字典
+    """
+    import json
+    body_dict=json.loads(body_str)
+    print(body_dict)
+    return HttpResponse('ok')
